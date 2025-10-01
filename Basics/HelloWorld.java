325bf644dcbb0ee606a4c9bdb83d9f9ad461bc8c
Darshan Prashanth <140430203+darshan-p-2508@users.noreply.github.com>
2025-10-01 23:41:36 +0530

    Create HelloWorld.java

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // Program execution starts from the main method
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the console, in the new line
+        System.out.println("Hello, World");
+    }
+}
